b06ea09bf6951400162f4c6c4efbcd6071938910
jnreynoso <jean.reynoso@inticode.com>
2018-10-13 19:23:04 -0500

    fix bug height

--- src/routes/welcome/tabs/index.js
+++ src/routes/welcome/tabs/index.js
@@ -6,25 +6,19 @@ import Account from './account'
 
 const { TabPane } = Tabs
 
-const Wrapper = styled.div`
-  height: 40px;
-`
-
 const Main = () => (
-  <Wrapper>
-    <Tabs defaultActiveKey='account' >
-      <TabPane
-        tab={
-          <span>
-            <Icon type='user' />Cuenta
-          </span>
-        }
-        key='account'
-      >
-        <Account />
-      </TabPane>
-    </Tabs>
-  </Wrapper>
+  <Tabs defaultActiveKey='account' >
+    <TabPane
+      tab={
+        <span>
+          <Icon type='user' />Cuenta
+        </span>
+      }
+      key='account'
+    >
+      <Account />
+    </TabPane>
+  </Tabs>
 )
 
 export default Main
